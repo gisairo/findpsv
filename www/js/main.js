f3e3bf85a0349f057e84ff08d5af544acbd5e73e
james brian <jamesgsr@gmail.com>
2016-06-26 21:10:59 +0300

    trying to get keyboard to close on submit and other debug items added

--- www/js/main.js
+++ www/js/main.js
@@ -74,5 +74,39 @@
 			$('#result').html('');
 			$('#result').append('<div class="list_item">'+'No results'+'</div> ');
 		}
-	})
+		console.log(Keyboard);
+	});
+	document.addEventListener('deviceready', onDeviceReady, false);
+
+	function onDeviceReady() {
+
+	    // Mock device.platform property if not available
+	    if (!window.device) {
+	        window.device = { platform: 'Browser' };
+	    }
+
+	    handleExternalURLs();
+	}
+
+	function handleExternalURLs() {
+	    // Handle click events for all external URLs
+	    if (device.platform.toUpperCase() === 'ANDROID') {
+	        $(document).on('click', 'a[href^="http"]', function (e) {
+	            var url = $(this).attr('href');
+	            navigator.app.loadUrl(url, { openExternal: true });
+	            e.preventDefault();
+	        });
+	    }
+	    else if (device.platform.toUpperCase() === 'IOS') {
+	        $(document).on('click', 'a[href^="http"]', function (e) {
+	            var url = $(this).attr('href');
+	            window.open(url, '_system');
+	            e.preventDefault();
+	        });
+	    }
+	    else {
+	        // Leave standard behaviour
+	    }
+	}
+	
 })(jQuery);
